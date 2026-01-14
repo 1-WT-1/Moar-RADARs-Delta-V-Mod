f3f6007b3d122cfe1b7cf481a1d5bdb9122a1b49
WT <go.to.another5@gmail.com>
2026-01-14 15:10:21 +0200

    v2.0.0 indev
    Doppler radars
    Switch to hevlib for adding to slots
    Manifest V2

---
+++ Moar RADARs/HEVLIB_EQUIPMENT_DRIVER_TAGS/ADD_EQUIPMENT_ITEMS.gd
@@ -0,0 +1,79 @@
+const SYSTEM_RADAR_DOPPLER = {
+	"system": "SYSTEM_RADAR_DOPPLER",
+	"name_override": "SYSTEM_RADAR_DOPPLER",
+	"description": "SYSTEM_RADAR_DOPPLER_DESC",
+	"specs": "SYSTEM_RADAR_DOPPLER_SPECS",
+	"manual": "SYSTEM_RADAR_DOPPLER_MANUAL",
+	"price": 20000,
+	"test_protocol": "lidar",
+	"slot_type": "LIDAR",
+	"equipment_type": "SENSOR_LIDAR",
+	"weapon_slot": {
+		"path": "res://Moar RADARs/enceladus/SYSTEM_RADAR_DOPPLER.tscn",
+		"data": []
+	}
+}
+
+const SYSTEM_RADAR_CONE = {
+	"system": "SYSTEM_RADAR_CONE",
+	"name_override": "SYSTEM_RADAR_CONE",
+	"description": "SYSTEM_RADAR_CONE_DESC",
+	"specs": "SYSTEM_RADAR_CONE_SPECS",
+	"manual": "SYSTEM_RADAR_CONE_MANUAL",
+	"price": 22500,
+	"test_protocol": "lidar",
+	"slot_type": "LIDAR",
+	"equipment_type": "SENSOR_LIDAR",
+	"weapon_slot": {
+		"path": "res://Moar RADARs/enceladus/SYSTEM_RADAR_CONE.tscn",
+		"data": []
+	}
+}
+
+const SYSTEM_RADAR_OPA = {
+	"system": "SYSTEM_RADAR_OPA",
+	"name_override": "SYSTEM_RADAR_OPA",
+	"description": "SYSTEM_RADAR_OPA_DESC",
+	"specs": "SYSTEM_RADAR_OPA_SPECS",
+	"manual": "SYSTEM_RADAR_OPA_MANUAL",
+	"price": 100000,
+	"test_protocol": "lidar",
+	"slot_type": "LIDAR",
+	"equipment_type": "SENSOR_LIDAR",
+	"weapon_slot": {
+		"path": "res://Moar RADARs/enceladus/SYSTEM_RADAR_OPA.tscn",
+		"data": []
+	}
+}
+
+const SYSTEM_RADAR_OPA_DOPPLER = {
+	"system": "SYSTEM_RADAR_OPA_DOPPLER",
+	"name_override": "SYSTEM_RADAR_OPA_DOPPLER",
+	"description": "SYSTEM_RADAR_OPA_DOPPLER_DESC",
+	"specs": "SYSTEM_RADAR_OPA_DOPPLER_SPECS",
+	"manual": "SYSTEM_RADAR_OPA_DOPPLER_MANUAL",
+	"price": 180000,
+	"test_protocol": "lidar",
+	"slot_type": "LIDAR",
+	"equipment_type": "SENSOR_LIDAR",
+	"weapon_slot": {
+		"path": "res://Moar RADARs/enceladus/SYSTEM_RADAR_OPA_DOPPLER.tscn",
+		"data": []
+	}
+}
+
+const SYSTEM_RADAR_HYBRID = {
+	"system": "SYSTEM_RADAR_HYBRID",
+	"name_override": "SYSTEM_RADAR_HYBRID",
+	"description": "SYSTEM_RADAR_HYBRID_DESC",
+	"specs": "SYSTEM_RADAR_HYBRID_SPECS",
+	"manual": "SYSTEM_RADAR_HYBRID_MANUAL",
+	"price": 400000,
+	"test_protocol": "lidar",
+	"slot_type": "LIDAR",
+	"equipment_type": "SENSOR_LIDAR",
+	"weapon_slot": {
+		"path": "res://Moar RADARs/enceladus/SYSTEM_RADAR_HYBRID.tscn",
+		"data": []
+	}
+}
